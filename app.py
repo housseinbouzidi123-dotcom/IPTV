ca3c2f13b6f1939a27f2b01e8131b1191bfa1c84
housseinbouzidi123-dotcom <houssein.bouzidi123@gmail.com>
2026-07-10 06:13:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 # رابط قناتك الجديدة والمحدثة مدمج هنا بشكل سليم مئة بالمئة
-URL = "http://217.60.15.181:8080/live///b0:99:d7:15:88:50/3090914536649669/318198.ts?token=yXzbaHc.fHya.X.cUydz.ydzzbUUfHX.X.y.FR.ts.7c14c38c3090576757ad0d2d0f7f5019dd0600a1e9c209b64298017137953fe1.5089.VmlyZ2luIE1lZGlh.MTg1LjE5MS4xMjYuMTI3"
+URL = "http://217.60.15.181:8080/live///b0:99:d7:15:88:50/3090914536649669/318198.ts"
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
